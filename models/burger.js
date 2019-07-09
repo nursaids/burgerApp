381fba9ce77d91db3bbb01d2ff87bd16a71553c6
nursaid <uznet@yahoo.com>
2019-07-09 09:26:14 -0700

    CRUD burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,33 +1,29 @@
-const orm = require ("../config/orm");
+var orm = require ("../config/orm");
 var burger = {
-/**
- * @param {Function} cb
- */
-all: function (cb){
-    orm.selectAll("burgers", function (res){
+all: function(cb){
+    orm.all("burgers", function(res){
         cb(res);
     });
 },
-/**
- * @param {String} burgerName
- * @param {Function} cb
- */
+create: function(cols, vals, cb){
+    orm.create("burgers", col, vals, function(res){
+        cb(res);
+    });
+},
 
- create: function (burgerName, cb){
-     orm.insertOne("burgers", ["burger_name", "devoured"], [burgerName, 0], function(res){
-         cb(res);
-     });
- },
+update: function ( objColVals, condition, cb){
+    orm.update("burgers", objColVals, condition, function(res){
+        cb(res);
+    });
 
- /**
-  * @param {String} burgerName
-  * @param {Function} cb
-  */
- devour: function (id, cb){
-     orm.updateOne("burgers", { devoured: 1}, "id = " + id, function (res){
-         cb(res);
-     });
- }
+},
+
+delete: function(condition, cb){
+    orm.delete("burgers", condition, function(res){
+        cb(res);
+    });
+}
+
 };
 
 module.exports = burger;
