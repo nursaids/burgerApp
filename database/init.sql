68a2fdb227d1a134d7108d5b8fd44ffca4c9c589
nursaid <uznet@yahoo.com>
2019-07-20 12:07:09 -0700

    said

--- database/init.sql
+++ database/init.sql
@@ -1,12 +1,14 @@
+DROP DATABASE burger_db;
 CREATE DATABASE burger_db;
+
 USE burger_db;
 drop table burgers;
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	ID int NOT NULL AUTO_INCREMENT,
 	name varchar (255) NOT NULL,
 	eaten boolean DEFAULT false,
-	PRIMARY KEY(id)
+	PRIMARY KEY(ID)
 );
 
 select * from burgers;
