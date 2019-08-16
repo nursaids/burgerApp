1b6806148fea68d3d540a5ef22bc2a9800e0172f
nursaid <uznet@yahoo.com>
2019-08-15 22:51:01 -0700

    burger.js update

--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,28 @@
+// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function (cb) {
-        orm.all("burgers", function (res) {
-            cb(res);
-        });
+    // function to select all burgers
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
     },
 
-    create: function (cols, vals, cb) {
-        orm.create("burgers", cols, vals, function (res) {
-            cb(res);
-        });
+    // function to insert one new data
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
     },
-    update: function (objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
-    },
-    delete: function (condition, cb) {
-        orm.delete("burgers", condition, function (res) {
-            cb(res);
-        });
+
+    // function to update one existing data
+    updateOne: function(burger_id, callback){
+      orm.updateOne("burgers",burger_id, function(res){
+        callback(res);
+      });
     }
-};
-
-module.exports = burger;
+  };
+  
+  // Export the database functions for the controller (burgers_controller.js).
+  module.exports = burger;
